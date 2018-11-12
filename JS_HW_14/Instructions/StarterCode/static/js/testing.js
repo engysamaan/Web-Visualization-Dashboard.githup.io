4e88178c4b00557fff2d9f258c5c14af79959886
engysamaan <40504997+engysamaan@users.noreply.github.com>
2018-11-11 19:39:52 -0500

    changes

--- JS_HW_14/Instructions/StarterCode/static/js/testing.js
+++
@@ -1,9 +0,0 @@
-    
-if (inputValue == String){
-        var selectCity = ufoData.filter(row => row.city === inputValue);
-        console.log(selectCity);
-        buildTable(selectCity);
-    } else if (inputValue == Date){
-        var selectDataTime = ufoData.filter(row => row.datetime === inputValue);
-        console.log(selectDataTime);
-        buildTable(selectDataTime);
